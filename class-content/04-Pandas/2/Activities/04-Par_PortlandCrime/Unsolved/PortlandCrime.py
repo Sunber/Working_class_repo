10ecb7f20bf74fc46b599e424ce9d6d470679f46
Abhi Koukutla <abhi1993@hotmail.com>
2018-08-11 14:16:13 -0500

    adding all files to new repo

---
+++ class-content/04-Pandas/2/Activities/04-Par_PortlandCrime/Unsolved/PortlandCrime.py
@@ -0,0 +1,53 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Import Dependencies
+import pandas as pd
+
+
+# In[ ]:
+
+
+# Reference the file where the CSV is located
+
+# Import the data into a Pandas DataFrame
+
+
+# In[ ]:
+
+
+# look for missing values
+
+
+# In[ ]:
+
+
+# drop null rows
+
+
+# In[ ]:
+
+
+# verify counts
+
+
+# In[ ]:
+
+
+# Check to see if there are any values with mispelled or similar values in "Offense Type"
+
+
+# In[ ]:
+
+
+# Combine similar offenses
+
+
+# In[ ]:
+
+
+# Create a new DataFrame that looks into a specific neighborhood
+
